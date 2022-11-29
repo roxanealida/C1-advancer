2b3de3e1c057e11977b7af86c7822c631c80de25
roxanealida <119406312+roxanealida@users.noreply.github.com>
2022-11-29 12:31:46 +0100

    updated main file

--- translator.py
+++ translator.py
@@ -1,39 +1,52 @@
+
+'''
+1. The objective is to build a dataframe that contains the Oxford 5000 C1 words 
+and their translations into French, Spanish, Portuguese and Arabic.
+2. Each language's translation gets written into a separate file
+2. From these files all pendants are arrayed into a dataframe
+'''
+
 from googletrans import Translator
-# import pandas as pd  
-import pandas as pd  
+import pandas as pd
+import arabic_reshaper
+from bidi.algorithm import get_display
 
+#First, the C1 Oxford words are written into the "English_words.txt" file.
+#By doing this, the POS information is excluded.
 
-'''
-1. Translate from Oxford 5000 C2 list
-2. Each Language is in a different file
-2. Array them into one dataframe
-
-'''
-
-# with open("Spanish_words.txt", "r", encoding="utf-8") as f:
-#     with open("French_words.txt", "r", encoding="utf-8") as g:
-#         with open("Portuguese_words.txt", "r", encoding= "utf-8") as h:
-#             with open("English_words.txt", "r", encoding="utf-8") as i:
-#                 data = {"English":[line[:-1] for line in i.readlines()], "French": [line[:-1] for line in g.readlines()],
-#                 "Spanish": [line[:-1] for line in f.readlines()], "Portuguese": [line[:-1] for line in h.readlines()]}
-
-# df = pd.DataFrame(data)
-# print(df)
-
-with open("Oxford_5000_C2.txt") as file:
+with open("Oxford_5000_C2.txt", encoding="utf-8") as file:
     lines = file.readlines()
     words = [line[0: line.find(' ')] for line in lines]
- 
 
 with open("English_words.txt", "w", encoding="utf-8") as i:
     for j in words:
         i.write(j+"\n")
 
+#The next step is to translate all English C1 words into the below languages.
+
 translator = Translator()
-
-
+words_fr = [translator.translate(word, dest = "fr").text for word in words]
+words_es = [translator.translate(word, dest = "es").text for word in words]
+words_pt = [translator.translate(word, dest = "pt").text for word in words]
 words_ar = [translator.translate(word, dest = "ar").text for word in words]
 
+#These words are now written into separate files.
+
+with open("Portuguese_words.txt", "w", encoding= "utf-8") as f:
+    for i in words_pt:
+        f.write(i)
+        f.write("\n")
+
+
+with open("Portuguese_words.txt", "w", encoding= "utf-8") as f:
+    for i in words_pt:
+        f.write(i)
+        f.write("\n")
+
+with open("Portuguese_words.txt", "w", encoding= "utf-8") as f:
+    for i in words_pt:
+        f.write(i)
+        f.write("\n")
 
 with open("Arabic_words.txt", "w", encoding= "utf-8") as f:
     for i in words_ar:
@@ -41,24 +54,20 @@ with open("Arabic_words.txt", "w", encoding= "utf-8") as f:
         f.write("\n")
 
 
+#and finally, all translations are arrayed into a dataframe.
 
 
-# words_pt = [translator.translate(word, dest = "pt").text for word in words]
+with open("Spanish_words.txt", "r", encoding="utf-8") as f:
+    with open("French_words.txt", "r", encoding="utf-8") as g:
+        with open("Portuguese_words.txt", "r", encoding= "utf-8") as h:
+            with open("English_words.txt", "r", encoding="utf-8") as i:
+                with open("Arabic_words.txt", "r", encoding = "UTF-8") as j:
+                    data = {"English":[line[:-1] for line in i.readlines()],
+                    "French": [line[:-1] for line in g.readlines()],
+                    "Spanish": [line[:-1] for line in f.readlines()],
+                    "Portuguese": [line[:-1] for line in h.readlines()],
+                    "Arabic": [get_display(arabic_reshaper.reshape(line[:-1]))
+                    for line in j.readlines()]}
 
-# with open("Portuguese_words.txt", "w", encoding= "utf-8") as f:
-#     for i in words_pt:
-#         f.write(i)
-#         f.write("\n")
-
-# for word in words:
-#     translation_to_french = translator.translate(word, dest = "fr")
-#     words_fr.append(translation_to_french.text)
-# print(words_fr)
-
-# print(len(words))
-# words_es = []
-# for word in words:
-#     translation_to_spanish = translator.translate(word, dest = "es")
-#     words_es.append(translation_to_french.text)
-# print(words_es)
-# print("Done")
+df = pd.DataFrame(data)
+print(df)
