9558018d65466fea85e99662ce2e0dc41dbedd5a
roxanealida <119406312+roxanealida@users.noreply.github.com>
2022-11-29 11:15:37 +0100

    deleted the tempCodeRunner file that was obsolete

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-with open("Spanish_words.txt", "r", encoding="utf-8") as f:
-#     with open("French_words.txt", "r", encoding="utf-8") as g:
-#         with open("Portuguese_words.txt", "r", encoding= "utf-8") as h:
-#             with open("English_words.txt", "r", encoding="utf-8" as i):
-#                 data = {"English":[line[:-1] for line in i.readlines()], "Spanish": [line[:-1] for line in f.readlines()], 
-#                 "French": [line[:-1] for line in g.readlines()], "Portuguese": [line[:-1] for line in h.readlines()]}
